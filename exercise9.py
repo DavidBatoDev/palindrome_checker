82a54b94101d295fcf2a5ccbdc4fc4aeb5557916
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 15:44:53 +0800

    Done with pseudo code

---
+++ exercise9.py
@@ -0,0 +1,8 @@
+# check if the number is a palindrome
+# define a function with a parameter of a number
+# takes the parameter of a number and create a variable that converted to a string
+# create a variable for reverse number
+# if number and reverse number is same:
+#   it is a palindrome number
+# otherwise:
+#   its not
